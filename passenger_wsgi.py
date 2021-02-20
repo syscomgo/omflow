365c3228fed9df99d44949ffbc2041f1b91dc579
Youssri Ahmed <youssri.ahmed@cg-eg.com>
2021-02-20 12:27:54 +0200

    adding arabic translation , add static urles in url.py to fix static files error

---
+++ passenger_wsgi.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python %s\n' % sys.version.split()[0]
+    response = '\n'.join([message, version])
+    return [response.encode()]
+
+
+import omflow.wsgi
+SCRIPT_NAME = '/home/aeraeg/python/'
+
+class PassengerPathInfoFix(object):
+    """
+    Sets PATH_INFO from REQUEST_URI because Passenger doesn't provide it.
+    """
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        from urllib.parse import unquote
+        environ['SCRIPT_NAME'] = SCRIPT_NAME
+
+        request_uri = unquote(environ['REQUEST_URI'])
+        script_name = unquote(environ.get('SCRIPT_NAME', ''))
+        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
+        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
+        return self.app(environ, start_response)
+
+application = omflow.wsgi.application
+application = PassengerPathInfoFix(application)
